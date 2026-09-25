dfd11453b06f067eed33b6b4d3437a0f7129960c
Atikur Rahman Rajib <atikurajib@gmail.com>
2026-09-25 23:33:31 +0600

    basic understanding with go

--- 4_constants/constants.go
+++ 4_constants/constants.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+var name1 string = "adam"
+
+func main() {
+	const name2 = "travis"
+
+	fmt.Println(name1)
+	fmt.Println(name2)
+
+	const (
+		port = 5000
+		host = "local host"
+	)
+
+	fmt.Println(port, host)
 }
