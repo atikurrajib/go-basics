a29acb98d3e8b39397857f3abbcf5f33532ec3c6
Atikur Rahman Rajib <atikurajib@gmail.com>
2026-09-26 01:06:35 +0600

    basic understanding with go

---
+++ 5_for/for.go
@@ -0,0 +1,38 @@
+package main
+
+func main() {
+
+	/* 	//while with for loop:
+	   	i := 1
+	   	for i <= 3 {
+	   		fmt.Println(i)
+	   		i = i + 1
+	   	} */
+
+	/* 	infinite loop
+	for {
+		println("1")
+	} */
+
+	/* 	//classic for loop
+
+	   	for i := 0; i <= 3; i++ {
+	   		fmt.Println(i)
+	   	} */
+
+	/* 	//loop with break:
+
+	   	for j := 1; j <= 9; j++ {
+	   		break
+	   		fmt.Println(j)
+	   	} */
+
+	/* 	//loop with continue
+	   	for k := 0; k <= 9; k++ {
+	   		if k == 6 {
+	   			continue
+	   		}
+	   		fmt.Println(k) // print 0 to 9 with skiping 6
+	   	} */
+
+}
