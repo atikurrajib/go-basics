f3291fae0a7a2c3b94ffb427aaefd812eaf55937
Atikur Rahman Rajib <atikurajib@gmail.com>
2026-09-25 23:11:37 +0600

    basic understanding with go

---
+++ 3_variables/variables.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name = "golang" // var name string = "golang"
+	fmt.Println(name)
+
+	var isAdult bool = true
+	fmt.Println(isAdult)
+
+	var age int = 30
+	fmt.Println(age)
+
+	// shorthand syntax
+	name2 := "golang"
+	fmt.Println(name2)
+
+	var price = 50.5 
+	// var price float = 50.5
+	// price := 50.5
+	fmt.Println(price)
+}
